35d33d4cb1d8642af0e131c1f76d6d6b77a99e85
JANA K <135352469+Janasmarty@users.noreply.github.com>
2025-11-06 14:51:11 +0530

    Update server.js

--- infohub-server/server.js
+++ infohub-server/server.js
@@ -8,7 +8,7 @@ import quotesRoutes from "./routes/quotes.js";
 dotenv.config();
 const app = express();
 
-app.use(cors({ origin: "http://localhost:5173" }));
+app.use(cors({ origin:"https://infohub-clien.onrender.com", "http://localhost:5173" }));
 app.use(express.json());
 
 app.use("/api/weather", weatherRoutes);
