735b5ff2ef278bfa4c0b562899d0e5334afcc1c9
JANA K <135352469+Janasmarty@users.noreply.github.com>
2025-11-06 14:45:42 +0530

    Update api.js

--- infohub-client/src/api/api.js
+++ infohub-client/src/api/api.js
@@ -3,10 +3,10 @@ import axios from "axios";
 const API_BASE = "https://infohub-server-eml6.onrender.com";
 
 export const fetchWeather = (city) =>
-  axios.get(`${API_BASE}/weather?city=${city}`);
+  axios.get(`${API_BASE}/api/weather?city=${city}`);
 
 export const fetchCurrency = () =>
-  axios.get(`${API_BASE}/currency`);
+  axios.get(`${API_BASE}/api/currency`);
 
 export const fetchQuote = () =>
-  axios.get(`${API_BASE}/quotes`);
+  axios.get(`${API_BASE}/api/quotes`);
